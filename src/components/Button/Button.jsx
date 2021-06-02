24cfbee1ca1f9f09cd13cc0924ac625514a2e2c4
Tatsiana Slapik <slapik.tatiana@gmail.com>
2021-06-03 00:23:40 +0300

    feat: added popup

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,19 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Cart from "../../assets/img/cart.png";
+import Popup from "../Popup/Popup";
 import "./Button.scss";
 
 export default function Button() {
-  return <button className="button">В корзину </button>;
+  const [showPopup, setShowPopup] = useState(false);
+  const show = () => setShowPopup(true);
+  const hide = () => setShowPopup(false);
+  return (
+    <div className="app">
+      <button className="button" onClick={show}>
+        <img src={Cart} alt="basket" />
+        <p>Add to cart</p>
+      </button>
+      {showPopup === true ? <Popup onHidePopup={hide}></Popup> : null}
+    </div>
+  );
 }
